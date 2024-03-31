c8b1f97edf3bf60e675f165159ea1cbdb306ed82
JDGarzon <93989744+JDGarzon@users.noreply.github.com>
2024-03-31 00:03:19 -0500

    update

--- next-app/src/gacha/gacha.controller.ts
+++ next-app/src/gacha/gacha.controller.ts
@@ -2,10 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { GachaService } from './gacha.service';
 import { CreateGachaDto } from './dto/create-gacha.dto';
 import { UpdateGachaDto } from './dto/update-gacha.dto';
+import { CharacterController } from 'src/character/character.controller';
+import { WeaponController } from 'src/weapon/weapon.controller';
 
 @Controller('gacha')
 export class GachaController {
-  constructor(private readonly gachaService: GachaService) {}
+  constructor(private readonly gachaService: GachaService,
+    private readonly charactersController:CharacterController,
+    private readonly weaponsController:WeaponController
+    ) {}
 
   @Post()
   create(@Body() createGachaDto: CreateGachaDto) {
@@ -31,4 +36,20 @@ export class GachaController {
   remove(@Param('id') id: string) {
     return this.gachaService.remove(+id);
   }
+
+  getOneCharacter(){
+
+  }
+
+  getTenCharacters(){
+  }
+
+  getOneWeapon(){
+
+  }
+
+  getTenWeapon(){
+  }
+
+
 }
